4d3bbe501c18c1f6a3b8a2611196cacb2b868f3f
Zaidbaidadekalb <87798207+Zaidbaidadekalb@users.noreply.github.com>
2021-10-03 17:10:11 -0400

    Main.Lua
    this is the main file of Zaidhub

---
+++ Owl Hub.lua
@@ -0,0 +1,466 @@
+-- Made by ZaidBaida#2129
+
+local NewGuihold = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local ZaidHub = Instance.new("TextLabel")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local Maintelekinesis = Instance.new("TextButton")
+local TextBox = Instance.new("TextBox")
+local ShindoLife = Instance.new("TextButton")
+local OK = Instance.new("TextButton")
+local DaHood = Instance.new("TextButton")
+local BeeSwarmSimulator = Instance.new("TextButton")
+local SkyWars = Instance.new("TextButton")
+local TheMimic = Instance.new("TextButton")
+local BedWars = Instance.new("TextButton")
+local WelcometoBloxburg = Instance.new("TextButton")
+local CloneTycoon2 = Instance.new("TextButton")
+local Fly = Instance.new("TextButton")
+local Arsenal = Instance.new("TextButton")
+local PetSimulatorX = Instance.new("TextButton")
+local Kat = Instance.new("TextButton")
+local StandsAwakening = Instance.new("TextButton")
+local TheLift = Instance.new("TextButton")
+local CounterBlox = Instance.new("TextButton")
+local lololol = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+local KeyFrame = Instance.new("Frame")
+local KeySystem = Instance.new("TextLabel")
+local Check = Instance.new("TextButton")
+local key = Instance.new("TextBox")
+
+
+
+NewGuihold.Name = " New Gui hold"
+NewGuihold.Parent = game.CoreGui
+
+Frame.Parent = NewGuihold
+Frame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+Frame.Position = UDim2.new(0.0890025645, 0, 0.22895211, 0)
+Frame.Size = UDim2.new(0, 1122, 0, 540)
+Frame.Visible = false
+Frame.Active = true
+Frame.Draggable = true 
+
+ZaidHub.Name = "ZaidHub"
+ZaidHub.Parent = Frame
+ZaidHub.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+ZaidHub.BackgroundTransparency = 0.400
+ZaidHub.Position = UDim2.new(-0.00178253104, 0, -0.00185185182, 0)
+ZaidHub.Size = UDim2.new(0, 1122, 0, 63)
+ZaidHub.Font = Enum.Font.SourceSans
+ZaidHub.LineHeight = 3.000
+ZaidHub.Text = "ZaidHub"
+ZaidHub.TextColor3 = Color3.fromRGB(0, 0, 0)
+ZaidHub.TextSize = 73.000
+ZaidHub.TextStrokeTransparency = 0.750
+ZaidHub.TextTransparency = 0.400
+ZaidHub.TextWrapped = true
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+TextLabel.Position = UDim2.new(0.76690352, 0, 0.114971727, 0)
+TextLabel.Size = UDim2.new(0, 139, 0, 50)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Speed Changer"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 28.000
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.Position = UDim2.new(0.195227861, 0, -0.351851821, 0)
+TextLabel_2.Size = UDim2.new(0, 664, 0, 174)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "This was made by ZaidBaida#2129"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextSize = 49.000
+TextLabel_2.TextWrapped = true
+
+Maintelekinesis.Name = "Main telekinesis"
+Maintelekinesis.Parent = Frame
+Maintelekinesis.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+Maintelekinesis.Position = UDim2.new(0.195695877, 0, 0.214532346, 0)
+Maintelekinesis.Size = UDim2.new(0, 200, 0, 50)
+Maintelekinesis.Font = Enum.Font.SourceSans
+Maintelekinesis.Text = "Telekinesis"
+Maintelekinesis.TextColor3 = Color3.fromRGB(0, 0, 0)
+Maintelekinesis.TextScaled = true
+Maintelekinesis.TextSize = 14.000
+Maintelekinesis.TextWrapped = true
+Maintelekinesis.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/ptj3LbfP", true))()
+end)
+
+TextBox.Parent = Frame
+TextBox.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+TextBox.Position = UDim2.new(0.767242849, 0, 0.226792485, 0)
+TextBox.Size = UDim2.new(0, 138, 0, 37)
+TextBox.Font = Enum.Font.Bodoni
+TextBox.PlaceholderColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.Text = ""
+TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.TextSize = 30.000
+
+ShindoLife.Name = "Shindo Life"
+ShindoLife.Parent = Frame
+ShindoLife.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+ShindoLife.Position = UDim2.new(0.394956827, 0, 0.365599543, 0)
+ShindoLife.Size = UDim2.new(0, 200, 0, 50)
+ShindoLife.Font = Enum.Font.SourceSans
+ShindoLife.Text = "Shindo Life"
+ShindoLife.TextColor3 = Color3.fromRGB(0, 0, 0)
+ShindoLife.TextScaled = true
+ShindoLife.TextSize = 14.000
+ShindoLife.TextStrokeTransparency = 0.900
+ShindoLife.TextWrapped = true
+ShindoLife.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet('https://solarishub.dev/script.lua',true))()
+end)
+
+OK.Name = "OK"
+OK.Parent = Frame
+OK.BackgroundColor3 = Color3.fromRGB(39, 50, 57)
+OK.Position = UDim2.new(0.754738271, 0, 0.331425726, 0)
+OK.Size = UDim2.new(0, 165, 0, 41)
+OK.Font = Enum.Font.Cartoon
+OK.Text = "APPLY"
+OK.TextColor3 = Color3.fromRGB(100, 100, 100)
+OK.TextSize = 42.000
+
+DaHood.Name = "Da Hood "
+DaHood.Parent = Frame
+DaHood.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+DaHood.Position = UDim2.new(0.394956827, 0, 0.214532346, 0)
+DaHood.Size = UDim2.new(0, 200, 0, 50)
+DaHood.Font = Enum.Font.SourceSans
+DaHood.Text = "Da Hood "
+DaHood.TextColor3 = Color3.fromRGB(0, 0, 0)
+DaHood.TextScaled = true
+DaHood.TextSize = 14.000
+DaHood.TextWrapped = true
+DaHood.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Biteyz/Smoke/main/source')))()
+end)
+
+BeeSwarmSimulator.Name = "Bee Swarm Simulator"
+BeeSwarmSimulator.Parent = Frame
+BeeSwarmSimulator.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+BeeSwarmSimulator.Position = UDim2.new(0.195695877, 0, 0.36745137, 0)
+BeeSwarmSimulator.Size = UDim2.new(0, 200, 0, 50)
+BeeSwarmSimulator.Font = Enum.Font.SourceSans
+BeeSwarmSimulator.Text = "Bee Swarm Simulator"
+BeeSwarmSimulator.TextColor3 = Color3.fromRGB(0, 0, 0)
+BeeSwarmSimulator.TextScaled = true
+BeeSwarmSimulator.TextSize = 14.000
+BeeSwarmSimulator.TextWrapped = true
+BeeSwarmSimulator.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/not-weuz/Lua/main/bsstrainer.lua"))()
+end)
+
+SkyWars.Name = "Sky Wars "
+SkyWars.Parent = Frame
+SkyWars.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+SkyWars.Position = UDim2.new(0.195695877, 0, 0.521939814, 0)
+SkyWars.Size = UDim2.new(0, 200, 0, 50)
+SkyWars.Font = Enum.Font.SourceSans
+SkyWars.Text = "Sky Wars"
+SkyWars.TextColor3 = Color3.fromRGB(0, 0, 0)
+SkyWars.TextScaled = true
+SkyWars.TextSize = 14.000
+SkyWars.TextWrapped = true
+SkyWars.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/Loadstring9VHD6Wmyi82n", true))()
+end)
+
+
+TheMimic.Name = "The Mimic \\n"
+TheMimic.Parent = Frame
+TheMimic.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+TheMimic.Position = UDim2.new(0.394448131, 0, 0.521939814, 0)
+TheMimic.Size = UDim2.new(0, 200, 0, 50)
+TheMimic.Font = Enum.Font.SourceSans
+TheMimic.Text = "The Mimic"
+TheMimic.TextColor3 = Color3.fromRGB(0, 0, 0)
+TheMimic.TextScaled = true
+TheMimic.TextSize = 14.000
+TheMimic.TextWrapped = true
+TheMimic.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/NCaKSm3N"))()
+end)
+
+
+BedWars.Name = "Bed Wars"
+BedWars.Parent = Frame
+BedWars.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+BedWars.Position = UDim2.new(-0.00038254261, 0, 0.214532375, 0)
+BedWars.Size = UDim2.new(0, 200, 0, 50)
+BedWars.Font = Enum.Font.SourceSans
+BedWars.Text = "Bed Wars"
+BedWars.TextColor3 = Color3.fromRGB(0, 0, 0)
+BedWars.TextScaled = true
+BedWars.TextSize = 14.000
+BedWars.TextWrapped = true
+BedWars.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet'https://dogix.wtf/scripts/noboline/a42fcf2c-c780-4077-8872-fa593f964db9')()
+end)
+
+
+WelcometoBloxburg.Name = "Welcome to Bloxburg"
+WelcometoBloxburg.Parent = Frame
+WelcometoBloxburg.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+WelcometoBloxburg.Position = UDim2.new(-0.00038254261, 0, 0.366384208, 0)
+WelcometoBloxburg.Size = UDim2.new(0, 200, 0, 50)
+WelcometoBloxburg.Font = Enum.Font.SourceSans
+WelcometoBloxburg.Text = "Welcome to Bloxburg"
+WelcometoBloxburg.TextColor3 = Color3.fromRGB(0, 0, 0)
+WelcometoBloxburg.TextScaled = true
+WelcometoBloxburg.TextSize = 14.000
+WelcometoBloxburg.TextWrapped = true
+WelcometoBloxburg.MouseButton1Down:connect(function()
+	
+end)
+
+
+CloneTycoon2.Name = "Clone Tycoon 2"
+CloneTycoon2.Parent = Frame
+CloneTycoon2.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+CloneTycoon2.Position = UDim2.new(-0.00038254261, 0, 0.521939814, 0)
+CloneTycoon2.Size = UDim2.new(0, 200, 0, 50)
+CloneTycoon2.Font = Enum.Font.SourceSans
+CloneTycoon2.Text = "Clone Tycoon 2"
+CloneTycoon2.TextColor3 = Color3.fromRGB(0, 0, 0)
+CloneTycoon2.TextScaled = true
+CloneTycoon2.TextSize = 14.000
+CloneTycoon2.TextWrapped = true
+CloneTycoon2.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://paste.ee/r/N0fo7/0"))()
+end)
+
+
+Fly.Name = "Fly"
+Fly.Parent = Frame
+Fly.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+Fly.Position = UDim2.new(0.739367962, 0, 0.473791659, 0)
+Fly.Size = UDim2.new(0, 200, 0, 50)
+Fly.Font = Enum.Font.SourceSans
+Fly.Text = "Fly"
+Fly.TextColor3 = Color3.fromRGB(0, 0, 0)
+Fly.TextScaled = true
+Fly.TextSize = 14.000
+Fly.TextWrapped = true
+Fly.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/QXbnfa5h", true))()
+end)
+
+Arsenal.Name = "Arsenal"
+Arsenal.Parent = Frame
+Arsenal.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+Arsenal.Position = UDim2.new(0.394448131, 0, 0.810828686, 0)
+Arsenal.Size = UDim2.new(0, 200, 0, 50)
+Arsenal.Font = Enum.Font.SourceSans
+Arsenal.Text = "Arsenal"
+Arsenal.TextColor3 = Color3.fromRGB(0, 0, 0)
+Arsenal.TextScaled = true
+Arsenal.TextSize = 14.000
+Arsenal.TextWrapped = true
+Arsenal.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet('http://www.beemoscripts.com/FlinchHub/Loader.lua'))();
+end)
+
+
+PetSimulatorX.Name = "Pet Simulator X"
+PetSimulatorX.Parent = Frame
+PetSimulatorX.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+PetSimulatorX.Position = UDim2.new(0.394448102, 0, 0.670087993, 0)
+PetSimulatorX.Size = UDim2.new(0, 200, 0, 50)
+PetSimulatorX.Font = Enum.Font.SourceSans
+PetSimulatorX.Text = "Pet Simulator X"
+PetSimulatorX.TextColor3 = Color3.fromRGB(0, 0, 0)
+PetSimulatorX.TextScaled = true
+PetSimulatorX.TextSize = 14.000
+PetSimulatorX.TextWrapped = true
+PetSimulatorX.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/XLinestX/Shiny-Tool/main/ShinyTool.lua"))()
+end)
+
+
+Kat.Name = "Kat  "
+Kat.Parent = Frame
+Kat.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+Kat.Position = UDim2.new(0.195695907, 0, 0.810828686, 0)
+Kat.Size = UDim2.new(0, 200, 0, 50)
+Kat.Font = Enum.Font.SourceSans
+Kat.Text = "Kat  Ez aim"
+Kat.TextColor3 = Color3.fromRGB(0, 0, 0)
+Kat.TextScaled = true
+Kat.TextSize = 14.000
+Kat.TextWrapped = true
+Kat.MouseButton1Down:connect(function()
+	loadstring(game:httpget("https://pastebin.com/raw/8Mt79PsC", true))()
+end)
+
+
+StandsAwakening.Name = "Stands Awakening"
+StandsAwakening.Parent = Frame
+StandsAwakening.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+StandsAwakening.Position = UDim2.new(0.195695907, 0, 0.67193979, 0)
+StandsAwakening.Size = UDim2.new(0, 200, 0, 50)
+StandsAwakening.Font = Enum.Font.SourceSans
+StandsAwakening.Text = "Stands Awakening"
+StandsAwakening.TextColor3 = Color3.fromRGB(0, 0, 0)
+StandsAwakening.TextScaled = true
+StandsAwakening.TextSize = 14.000
+StandsAwakening.TextWrapped = true
+StandsAwakening.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Bebo-Mods/BeboScripts/main/StandAwekening.lua"))()
+end)
+
+
+TheLift.Name = "The Lift"
+TheLift.Parent = Frame
+TheLift.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+TheLift.Position = UDim2.new(-0.00127379328, 0, 0.67193979, 0)
+TheLift.Size = UDim2.new(0, 200, 0, 50)
+TheLift.Font = Enum.Font.SourceSans
+TheLift.Text = "The Lift"
+TheLift.TextColor3 = Color3.fromRGB(0, 0, 0)
+TheLift.TextScaled = true
+TheLift.TextSize = 14.000
+TheLift.TextWrapped = true
+TheLift.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/timkaMLG1/TheLiftFucker/main/main.lua",true))()
+end)
+
+
+CounterBlox.Name = "Counter Blox"
+CounterBlox.Parent = Frame
+CounterBlox.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+CounterBlox.Position = UDim2.new(-0.000382527709, 0, 0.810828686, 0)
+CounterBlox.Size = UDim2.new(0, 200, 0, 50)
+CounterBlox.Font = Enum.Font.SourceSans
+CounterBlox.Text = "Counter Blox"
+CounterBlox.TextColor3 = Color3.fromRGB(0, 0, 0)
+CounterBlox.TextScaled = true
+CounterBlox.TextSize = 14.000
+CounterBlox.TextWrapped = true
+CounterBlox.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Pawel12d/hexagon/main/scripts/CounterBlox.lua', true))()
+end)
+
+
+lololol.Name = "lololol"
+lololol.Parent = NewGuihold
+lololol.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+lololol.BackgroundTransparency = 1.000
+lololol.Position = UDim2.new(0, 0, 0.94040525, 0)
+lololol.Size = UDim2.new(0, 1408, 0, 50)
+lololol.Font = Enum.Font.SourceSans
+lololol.Text = "For The Telekinesis make sure that the part is unanchored"
+lololol.TextColor3 = Color3.fromRGB(0, 0, 0)
+lololol.TextSize = 54.000
+
+TextLabel_3.Parent = NewGuihold
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.Position = UDim2.new(0.287586749, 0, 0, 0)
+TextLabel_3.Size = UDim2.new(0, 636, 0, 50)
+TextLabel_3.Font = Enum.Font.SourceSans
+TextLabel_3.Text = "\"K \" Is Open And Close The Gui"
+TextLabel_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.TextSize = 26.000
+
+KeyFrame.Name = " Key Frame"
+KeyFrame.Parent = NewGuihold
+KeyFrame.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+KeyFrame.Position = UDim2.new(0.373939872, 0, 0.408588976, 0)
+KeyFrame.Size = UDim2.new(0, 411, 0, 233)
+
+KeySystem.Name = "Key System"
+KeySystem.Parent = KeyFrame
+KeySystem.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+KeySystem.BackgroundTransparency = 1.000
+KeySystem.Size = UDim2.new(0, 411, 0, 61)
+KeySystem.Font = Enum.Font.SourceSans
+KeySystem.Text = "Key System"
+KeySystem.TextColor3 = Color3.fromRGB(0, 0, 0)
+KeySystem.TextSize = 58.000
+KeySystem.TextWrapped = true
+
+Check.Name = "Check"
+Check.Parent = KeyFrame
+Check.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+Check.Position = UDim2.new(0.153284669, 0, 0.626609445, 0)
+Check.Size = UDim2.new(0, 295, 0, 59)
+Check.Font = Enum.Font.SourceSans
+Check.Text = "Check"
+Check.TextColor3 = Color3.fromRGB(255, 255, 255)
+Check.TextSize = 41.000
+Check.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+
+key.Name = "key"
+key.Parent = KeyFrame
+key.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+key.Position = UDim2.new(0.0754257888, 0, 0.429184556, 0)
+key.Size = UDim2.new(0, 358, 0, 32)
+key.Font = Enum.Font.SourceSans
+key.PlaceholderText = "Type Your key here"
+key.Text = ""
+key.TextColor3 = Color3.fromRGB(0, 0, 0)
+key.TextSize = 14.000
+
+-- Scripts:
+
+local function QHVF_fake_script() -- OK.LocalScript 
+	local script = Instance.new('LocalScript', OK)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		local plr = game.Players.LocalPlayer
+	    local char = plr.Character
+	    local hum = char.Humanoid
+		hum.WalkSpeed = script.Parent.Parent.TextBox.Text
+	end)
+end
+coroutine.wrap(QHVF_fake_script)()
+local function PNIJG_fake_script() -- Frame.LocalScript 
+	local script = Instance.new('LocalScript', Frame)
+
+	function PopupGui()
+		if script.Parent.Visible==true then script.Parent.Visible=false
+		else script.Parent.Visible=true
+		end
+	end
+	
+	
+	game:GetService("UserInputService").InputBegan:Connect(function(key)
+		if key.KeyCode==Enum.KeyCode.K then
+			PopupGui()
+		end
+	end)
+end
+coroutine.wrap(PNIJG_fake_script)()
+local function JKYR_fake_script() -- Check.LocalScript 
+	local script = Instance.new('LocalScript', Check)
+
+	local key = script.Parent.Parent.key
+	
+	script.Parent.MouseButton1Click:Connect(function()
+		if key.Text == "ZaidHub" then -- Make the "Key" whatever you wish.
+			script.Parent.Parent:TweenPosition(UDim2.new(0.383,0,-0.9,0), "Out", "Quad", 1, true)
+			script.Parent.Parent.Parent.Frame.Visible = true
+			wait(5)
+			script.Parent.Parent:Destroy() -- Destroys the GUI after a set time
+			print("Destroyed!")
+		elseif -- Tweening | If you want it to do a different tween just change the "Quad" to another tween animation
+			key.Text == "" then
+			key.Text = "" else
+			key.Text = "Incorrect, try again."
+			wait(1)
+			key.Text = ""
+		end
+	end)
+	
+	-- Video: https://youtu.be/hy1Tcwr3PRY
+end
+coroutine.wrap(JKYR_fake_script)()
